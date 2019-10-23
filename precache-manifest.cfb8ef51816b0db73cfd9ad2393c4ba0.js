a8667bc797b5369cf1d005966e8115bfc3189ef7
Nikoletta Domak <nikoletta.domak@accenture.com>
2019-10-23 12:05:43 +0200

    Updates

---
+++ precache-manifest.cfb8ef51816b0db73cfd9ad2393c4ba0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea22a3a1f660805e5a003e1f46c436c2",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "569ff161b28f2bc81054",
+    "url": "/crwn-clothing/static/css/main.15a1d7a6.chunk.css"
+  },
+  {
+    "revision": "6af5109ab334e4c2664e",
+    "url": "/crwn-clothing/static/js/2.ee36ca8f.chunk.js"
+  },
+  {
+    "revision": "569ff161b28f2bc81054",
+    "url": "/crwn-clothing/static/js/main.0fc0d36a.chunk.js"
+  },
+  {
+    "revision": "ddc729574f128ccd2719",
+    "url": "/crwn-clothing/static/js/runtime~main.20c21b29.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
